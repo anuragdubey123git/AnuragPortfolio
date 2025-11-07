adaee5c68012866c5df86759966b7daabc26a68d
Anurag Dubey <anuragdubey9651@gmail.com>
2025-11-07 18:59:03 +0530

    Update resume link in constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/anuragdubey123git",
   resume:
-    "https://drive.google.com/file/d/1TzWqkhXhf_DQVpOnWqZKZTmUssCZo2RL/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1o9tB-gDehkIYMAEvliinHgEuIUP0B-zk/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/anuragdubey75100/",
   twitter: "https://x.com/AnuragDube97416?t=1q-R1XtGYwPiROKOK6_PUg&s=09",
   insta: "https://www.instagram.com/who_anuragdubey/",
